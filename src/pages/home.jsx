73930d2032c43f9d43a1d52707b4fa7bb2c1c5d2
Jeferson Ruiz <jefersonruiz@URCLCORLMGTX.local>
2023-01-14 20:19:58 -0500

    Maquetación home.
    Se agregan los textos a la maquetación del home, junto con los estilos
    iniciales.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,24 @@
+import { useContext } from "react";
+import { LangContext } from "../contexts/langContext";
+import styles from '../styles/home.module.sass';
+
 function Home() {
+  const langCtx = useContext(LangContext);
+  const pageContent = langCtx.content.home;
+  let paragraphs = pageContent.content.map((item, index) => (
+    <p className="indent" key={index}>
+        {item}
+    </p>
+  ));
   return (
-    <div>Home Page</div>
+    <main className={`${styles.main}`} >
+      <div></div>
+      <div className={`${styles.container}`} >
+        <div>{pageContent.title}</div>
+        {paragraphs}
+      </div>
+    </main>
+
   );
 }
 export default Home;
